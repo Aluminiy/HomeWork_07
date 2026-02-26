6ebcfd91e95bfffc9558d3fb508e8ea085e713a1
Dmitrii Kaluzhin <aluminiy2713@gmail.com>
2026-02-26 22:01:26 +0300

    Pie chart impl

--- app/src/main/java/otus/homework/customview/MainActivity.kt
+++ app/src/main/java/otus/homework/customview/MainActivity.kt
@@ -1,11 +1,34 @@
 package otus.homework.customview
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import otus.homework.customview.databinding.ActivityMainBinding
+import org.json.JSONArray
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val jsonString = resources.openRawResource(R.raw.payload).bufferedReader().use { it.readText() }
+        val jsonArray = JSONArray(jsonString)
+        val data = mutableMapOf<String, Int>()
+
+        for (i in 0 until jsonArray.length()) {
+            val item = jsonArray.getJSONObject(i)
+            val category = item.getString("category")
+            val amount = item.getInt("amount")
+            data[category] = data.getOrDefault(category, 0) + amount
+        }
+
+        binding.pieChart.setData(data)
+        binding.pieChart.setOnSegmentClickListener { category ->
+            Toast.makeText(this, "Clicked on: $category", Toast.LENGTH_SHORT).show()
+        }
     }
 }
